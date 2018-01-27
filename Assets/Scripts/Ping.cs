c31213644a87714619bc863e21f251c2e63848f2
Paul Calande <paul.luap09@gmail.com>
2018-01-27 13:55:46 -0500

    pings now stick to walls and fade

--- Assets/Scripts/Ping.cs
+++ Assets/Scripts/Ping.cs
@@ -10,20 +10,58 @@ public class Ping : MonoBehaviour
     [SerializeField]
     [Tooltip("How many seconds the ping exists before getting destroyed.")]
     float lifetime;
+    [SerializeField]
+    [Tooltip("How many seconds to wait before the fade-out begins.")]
+    float fadeOutDelaySeconds;
+    [SerializeField]
+    [Tooltip("How many seconds long the fade-out is.")]
+    float fadeOutSeconds;
     /*
     [SerializeField]
     [Tooltip("The prefab to use for the light.")]
     GameObject prefabLight;
     */
+    [SerializeField]
+    [Tooltip("Reference to the rendering component.")]
+    SpriteRenderer render;
+    [SerializeField]
+    [Tooltip("Reference to the rigidbody component.")]
+    Rigidbody2D rb;
 
+    bool fadingOut = false;
     float secondsLived = 0.0f;
+    float fadeOutDelaySecondsPassed = 0.0f;
+    float fadeOutSecondsPassed = 0.0f;
 
     private void Update()
     {
-        secondsLived += Time.deltaTime;
         if (secondsLived > lifetime)
         {
             Terminate();
+        }
+        secondsLived += Time.deltaTime;
+
+        if (fadingOut)
+        {
+            if (fadeOutDelaySecondsPassed > fadeOutDelaySeconds)
+            {
+                float alpha = 1.0f - fadeOutSecondsPassed / fadeOutSeconds;
+                Color col = render.color;
+                col.a = alpha;
+                render.color = col;
+                if (fadeOutSecondsPassed > fadeOutSeconds)
+                {
+                    Destroy(gameObject);
+                }
+                else
+                {
+                    fadeOutSecondsPassed += Time.deltaTime;
+                }
+            }
+            else
+            {
+                fadeOutDelaySecondsPassed += Time.deltaTime;
+            }
         }
     }
 
@@ -40,7 +78,10 @@ public class Ping : MonoBehaviour
 
     private void Terminate()
     {
+        rb.constraints |= RigidbodyConstraints2D.FreezePosition;
+        rb.velocity = Vector2.zero;
+        fadingOut = true;
         //GameObject.Instantiate(prefabLight, transform.position + Vector3.back, Quaternion.identity);
-        Destroy(gameObject);
+        //Destroy(gameObject);
     }
 }
