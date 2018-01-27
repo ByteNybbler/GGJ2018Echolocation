cbb8903c8a645a0f7f44b58f167c42f08f00f9b5
Paul Calande <paul.luap09@gmail.com>
2018-01-27 15:28:20 -0500

    sprite renderer disabling script

---
+++ Assets/Scripts/DisableSpriteRenderer.cs
@@ -0,0 +1,18 @@
+﻿// Author(s): Paul Calande
+// Disables SpriteRenderer on Awake.
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DisableSpriteRenderer : MonoBehaviour
+{
+    [SerializeField]
+    [Tooltip("Reference to the rendering component.")]
+    SpriteRenderer render;
+
+    private void Start()
+    {
+        render.enabled = false;
+    }
+}
