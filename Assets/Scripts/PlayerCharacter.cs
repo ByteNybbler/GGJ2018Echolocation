626470ce0313aa708df4010a2f70fea461b7c15c
Paul Calande <paul.luap09@gmail.com>
2018-01-26 20:48:17 -0500

    started scripts and scene

---
+++ Assets/Scripts/PlayerCharacter.cs
@@ -0,0 +1,23 @@
+﻿// Author(s): Paul Calande
+// Script for player character in Echolocation.
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCharacter : MonoBehaviour
+{
+    [SerializeField]
+    [Tooltip("Reference to the Rigidbody.")]
+    Rigidbody2D rb;
+
+    private void Update()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector2 move = new Vector2(moveHorizontal, moveVertical);
+
+        rb.AddForce(move);
+    }
+}
