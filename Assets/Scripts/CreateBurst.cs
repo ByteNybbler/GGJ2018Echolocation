572a25af16eb2c602c99e0fddc34d9238884b00c
Paul Calande <paul.luap09@gmail.com>
2018-01-27 10:40:41 -0500

    added pinging

---
+++ Assets/Scripts/CreateBurst.cs
@@ -0,0 +1,33 @@
+﻿// Author(s): Paul Calande
+// Creates a series of objects that moves outwards in a circle.
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreateBurst : MonoBehaviour
+{
+    [SerializeField]
+    [Tooltip("The object to instantiate.")]
+    GameObject obj;
+    [SerializeField]
+    [Tooltip("The number of objects to instantiate.")]
+    int count;
+    [SerializeField]
+    [Tooltip("The speed at which the objects will move.")]
+    float speed;
+
+    // Create the objects.
+    public void Burst()
+    {
+        float degreeDifference = 360.0f / count;
+        float degreesSoFar = 0.0f;
+        for (int i = 0; i < count; ++i)
+        {
+            GameObject newObj = GameObject.Instantiate(obj, transform.position, Quaternion.identity);
+            Rigidbody2D rb = newObj.GetComponent<Rigidbody2D>();
+            rb.velocity = Quaternion.AngleAxis(degreesSoFar, Vector3.forward) * Vector2.up * speed;
+            degreesSoFar += degreeDifference;
+        }
+    }
+}
--- Assets/Scripts/PlayerCharacter.cs
+++ Assets/Scripts/PlayerCharacter.cs
@@ -8,8 +8,11 @@ using UnityEngine;
 public class PlayerCharacter : MonoBehaviour
 {
     [SerializeField]
-    [Tooltip("Reference to the Rigidbody.")]
+    [Tooltip("Reference to the Rigidbody component.")]
     Rigidbody2D rb;
+    [SerializeField]
+    [Tooltip("Reference to the CreateBurst component.")]
+    CreateBurst createBurst;
 
     private void Update()
     {
@@ -19,5 +22,10 @@ public class PlayerCharacter : MonoBehaviour
         Vector2 move = new Vector2(moveHorizontal, moveVertical);
 
         rb.AddForce(move);
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            createBurst.Burst();
+        }
     }
 }
