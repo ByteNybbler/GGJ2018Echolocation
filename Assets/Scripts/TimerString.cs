816de4154d102e8dc901fafd655cfd6d5b1de104
Paul Calande <paul.luap09@gmail.com>
2018-01-28 11:41:07 -0500

    started timer work

---
+++ Assets/Scripts/TimerString.cs
@@ -0,0 +1,15 @@
+﻿// Author(s): Paul Calande
+// Component that converts a number of seconds into a "digital time" string.
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimerString : MonoBehaviour
+{
+    public string Get(float rawSeconds)
+    {
+        int seconds = Mathf.FloorToInt(rawSeconds);
+        return "";
+    }
+}
