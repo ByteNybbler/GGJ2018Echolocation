3da74d1e7aa2a1b6bd1c5a30e7026c2335ad7fee
Paul Calande <paul.luap09@gmail.com>
2018-01-28 12:00:06 -0500

    timer string progress

--- Assets/Scripts/TimerString.cs
+++ Assets/Scripts/TimerString.cs
@@ -10,6 +10,24 @@ public class TimerString : MonoBehaviour
     public string Get(float rawSeconds)
     {
         int seconds = Mathf.FloorToInt(rawSeconds);
-        return "";
+        int secondsOnly = seconds % 60;
+        int minutesOnly = seconds / 60;
+
+        string result = "";
+        result += minutesOnly;
+        result += ":";
+        result += AddZeroPadding(secondsOnly, 2);
+        return result;
+    }
+
+    // Adds any 0s as padding if necessary.
+    private string AddZeroPadding(int val, int stringLength)
+    {
+        string result = val.ToString();
+        while (result.Length < stringLength)
+        {
+            result = "0" + result;
+        }
+        return result;
     }
 }
